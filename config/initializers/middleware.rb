0eb3655a3635eac0d09534b7de4d60ab994ec81c
John Paul <johnarpaul@gmail.com>
2023-06-15 15:41:47 +0100

    Update middleware.rb

--- config/initializers/middleware.rb
+++ config/initializers/middleware.rb
@@ -2,8 +2,8 @@
 
 server_name = ENV["PHLEXING_HOST"] || "www.phlexing.fun"
 
-if server_name && !Rails.env.development?
-  Rails.application.config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
-    r301(/.*/, "https://#{server_name}$&", if: proc { |rack_env| rack_env["SERVER_NAME"] != server_name })
-  end
-end
+# if server_name && !Rails.env.development?
+#   Rails.application.config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
+#     r301(/.*/, "https://#{server_name}$&", if: proc { |rack_env| rack_env["SERVER_NAME"] != server_name })
+#   end
+# end
